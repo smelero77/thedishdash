95b8b5dfba93a828ff4b12e4f898140a8df4200c
Tu Nombre <tucorreo@ejemplo.com>
2025-05-28 21:09:47 +0200

    Carrito independiente FUNCIONANDO

--- components/ScrollProgressBar.tsx
+++ components/ScrollProgressBar.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import { cn } from '@/utils/cn';
 
 interface ScrollProgressBarProps {
-  containerRef?: React.RefObject<HTMLElement>;
+  containerRef?: React.RefObject<HTMLDivElement>;
   className?: string;
 }
 
